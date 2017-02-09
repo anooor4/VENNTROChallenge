b88c5d6d23709c7935d1feb1aff7ca285b757f6a
anooor4 <a.nasir@hotmail.co.uk>
2017-02-09 15:49:11 +0000

    Form Authentication- Successful Login Test

--- VENNTROChallenge/src/FormAuthentication.java
+++ VENNTROChallenge/src/FormAuthentication.java
@@ -1,8 +1,25 @@
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class FormAuthentication {
 	
+	@Test
 	public void SuccessfulLogin(){
 		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://the-internet.herokuapp.com/login");
+		driver.findElement(By.xpath("//*[@id='username']")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//*[@id='password']")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.xpath("//*[@id='login']/button")).click();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		String successmsg = driver.findElement(By.xpath(".//*[@id='flash']")).getText();
+		Assert.assertTrue(successmsg.contains("You logged into a secure area!")); 
+		System.out.println(driver.findElement(By.xpath(".//*[@id='flash']")).getText());
 	}
 
 }
