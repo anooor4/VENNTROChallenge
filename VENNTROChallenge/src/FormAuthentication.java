d24bdb4eb899b43599304076b204af930f41b2d5
anooor4 <a.nasir@hotmail.co.uk>
2017-02-09 16:05:09 +0000

    Form Authentication-Unsuccessful Login Test

--- VENNTROChallenge/src/FormAuthentication.java
+++ VENNTROChallenge/src/FormAuthentication.java
@@ -21,5 +21,20 @@ public class FormAuthentication {
 		Assert.assertTrue(successmsg.contains("You logged into a secure area!")); 
 		System.out.println(driver.findElement(By.xpath(".//*[@id='flash']")).getText());
 	}
+	
+	@Test
+	public void UnsuccessfulLogin(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://the-internet.herokuapp.com/login");
+		driver.findElement(By.xpath("//*[@id='username']")).sendKeys("abcd");
+		driver.findElement(By.xpath("//*[@id='password']")).sendKeys("xyz");
+		driver.findElement(By.xpath("//*[@id='login']/button")).click();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		String errormsg = driver.findElement(By.xpath(".//*[@id='flash']")).getText();
+		Assert.assertTrue(errormsg.contains("Your username is invalid!"));
+		System.out.println(driver.findElement(By.xpath(".//*[@id='flash']")).getText());
+		
+	}
 
 }
